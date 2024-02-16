3fd82eec21421d222035599c2f507fce46cf99ce
Sai Shanker Jaina <156449266+SaiShankerJaina@users.noreply.github.com>
2024-02-15 19:00:16 -0500

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -1,28 +1,41 @@
-"use strict";
-
-/*
- * A simple Node.js program for exporting the current working directory via a
- * webserver listing on a hard code (see portno below) port. To start the
- * webserver run the command: node webServer.js
- *
- * Note that anyone able to connect to localhost:3001 will be able to fetch any
- * file accessible to the current user in the current directory or any of its
- * children.
- */
-
-/* jshint node: true */
-
-var express = require("express");
-
-var portno = 3000; // Port number to use
-
-var app = express();
-
-// We have the express static module
-// (http://expressjs.com/en/starter/static-files.html) do all the work for us.
-app.use(express.static(__dirname));
-
-var server = app.listen(portno, function () {
-  var port = server.address().port;
-  console.log("Listening at http://localhost:" + port + " exporting the directory " + __dirname);
-});
+module.exports = {
+  entry: {
+    gettingStarted: "./gettingStarted.jsx",
+    p2: "./p2.jsx",
+    p4:"./p4.jsx",
+    p5:"./p5.jsx",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: `${__dirname}/compiled`,
+    publicPath: "/",
+    filename: "[name].bundle.js",
+  },
+  mode: "development",
+};
