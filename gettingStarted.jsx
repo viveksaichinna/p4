51c0097a2a033b34c033a7a1c4e003d1d50f5332
Sai Shanker Jaina <156449266+SaiShankerJaina@users.noreply.github.com>
2024-02-16 10:53:52 -0500

    Update gettingStarted.jsx

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
+import Header from './components/header/Header';
 
 import Example from './components/example/Example';
 
 ReactDOM.render(
-  <Example />,
+  <div>
+      <Header/>
+      <Example />
+  </div>,
+  
+
   document.getElementById('reactapp'),
 );
